d277f034af748451270a3adc9a9b66e7337836bf
Galc3882 <86870298+Galc3882@users.noreply.github.com>
2023-09-03 10:45:51 -0400

    feat: initial layout style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      dark: "#2A2C2E",
+      midDark: "#2E2E2E",
+      darker: "#1B1C1D",
+      card: "#222222",
+      button: "#424242",
+      blue: "#2525B9",
+      white: "#ffffff",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      "cool-pattern": "url('images/cool-pattern.svg')",
+      boxShadow: {
+        "2xl": "-4px 4px 8px 0px #242424",
+        "3xl-inner": "inset 5px 5px 15px 5px #171717",
+      },
+      screens: {
+        short: { raw: "(min-height: 800px)" },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
